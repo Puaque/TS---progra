8d43096228f47e2b937564264e8043185a030c67
Puaque <143530922+Puaque@users.noreply.github.com>
2023-09-07 08:03:18 -0600

    Add files via upload

---
+++ TS5-JAPG_1189523.py
@@ -0,0 +1,17 @@
+"""Trabajo supervisado de prográmación
+7/09/23
+José Andrés Puaque Guerra"""
+
+sNombre = input (" Ingrese su nombre ")
+sEdad = input (" Ingrese su edad ")
+sCarrera = input (" Ingrese su carrera ")
+sCarne = input (" Ingrese su carne ")
+
+print("Mi segundo programa")
+print("Nombre: "+sNombre)
+print("Edad: "+sEdad)
+print("Carrera: "+sCarrera)
+print("Carné: "+sCarne)
+
+print("Soy " + sNombre+","+" tengo "+sEdad+" años y estudio la carrera de  "+sCarrera)
+print("Mi número de carné es "+sCarne)
